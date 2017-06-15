35622af5dbf42a75d3f5962a0c5a5b8985e03a35
megane42 <h.kzm.gm@gmail.com>
2017-06-15 02:07:49 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+var builder = require('botbuilder');
+var restify = require('restify');
+
+// auth for connector
+
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+// making server
+
+var server = restify.createServer();
+server.listen(3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+server.post('/api/messages', connector.listen());
+
+// making bot
+
+var bot = new builder.UniversalBot(connector);
+bot.dialog('/', function (session) {
+    session.send('Hello World');
+});
